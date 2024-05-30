f5aed2fcb5075687e64816d3ef01469381be25fa
Carlos Augusto da Silva Souza <carlos.a.s.souza321@gmail.com>
2024-05-30 14:29:32 -0300

    Ajuste do visual e semântica mais aperfeiçoada.

--- aqueogenese/index.php
+++ aqueogenese/index.php
@@ -21,114 +21,116 @@ include_once("template/header.php");
         Provident, maiores perferendis. Laboriosam pariatur incidunt eveniet tenetur quibusdam excepturi doloribus consequuntur aspernatur non!</p>
     </div>
   </div>
-
-  <div class="container-md mt-3">
-    <div class="row container-md">
-      <div class="col-md-6" id="car">
-        <!-- Carrossel -->
-        <div class="carousel-inner">
-          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div id="carousel-inner" class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="image/coli.avif" class="d-flex w-100 h-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="image/piram.avif" class="d-block w-100 h-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="image/greci.jpg" class="d-block w-100 h-100" alt="...">
+  
+  <article>
+    <div class="container-md mt-3">
+      <div class="row container-md">
+        <div class="col-md-6" id="car">
+          <!-- Carrossel -->
+          <div class="carousel-inner">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+              <div id="carousel-inner" class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="image/coli.avif" class="d-flex w-100 h-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="image/piram.avif" class="d-block w-100 h-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="image/greci.jpg" class="d-block w-100 h-100" alt="...">
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="col-md-6" id="letra">
         <!-- Texto ao lado do carrossel -->
-        <h2 id="text_lado">Texto ao lado do carrossel</h2>
+        <div class="col-md-6" id="letra">
+          <h2 id="text_lado">Texto ao lado do carrossel</h2>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
-          dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti atque quaerat, voluptates, accusantium laborum
+            dolorum culpa tenetur explicabo unde inventore nulla reprehenderit aliquam adipisci esse sapiente? Sunt reiciendis mollitia quos..
+          </p>
+        </div>
 
       </div>
-    </div>
+  </article>
 
-    <div id="titulo" class="container-md justify-content-center text-center m-3">
-      <h3>Pontos históricos e Pesquisa</h3>
-    </div>
+  <div id="titulo" class="container-md justify-content-center text-center m-3">
+    <h3>Pontos históricos e Pesquisa</h3>
+  </div>
 
-    <div id="contai" class="container-md">
-      <div class="container-md text-center">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 grid gap-0 row-gap-3">
-          <div class="col">
-            <div class="card">
-              <img id="ths" src="image/13.jpg" class="card-img-top" alt="...">
-              <div id="card1" class="card-body">
-                <h5 class="card-title">Árqueologia</h5>
-                <p class="card-text">Aqui você pode encontrar todas as escavações que atualmente a ArqueoGênese está atuando.</p>
-                <a href="#" class="btn">Conheça já</a>
-              </div>
+  <div id="contai" class="container-md">
+    <div class="container-md text-center">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 grid gap-0 row-gap-3">
+        <div class="col">
+          <div class="card">
+            <img id="ths" src="image/13.jpg" class="card-img-top" alt="...">
+            <div id="card1" class="card-body">
+              <h5 class="card-title">Árqueologia</h5>
+              <p class="card-text">Aqui você pode encontrar todas as escavações que atualmente a ArqueoGênese está atuando.</p>
+              <a href="#" class="btn">Conheça já</a>
             </div>
           </div>
-          <div class="col">
-            <div id="card1" class="card">
-              <img id="ths" src="image/transferir.jpeg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Monumentos</h5>
-                <p class="card-text">Aqui você tem contudos históricos sobre os monumentos e o povo que o construiu.</p>
-                <a href="#" class="btn">Conheça já</a>
-              </div>
+        </div>
+        <div class="col">
+          <div id="card1" class="card">
+            <img id="ths" src="image/transferir.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Monumentos</h5>
+              <p class="card-text">Aqui você tem contudos históricos sobre os monumentos e o povo que o construiu.</p>
+              <a href="#" class="btn">Conheça já</a>
             </div>
           </div>
-          <div class="col">
-            <div id="card1" class="card">
-              <img id="ths" src="image/fosseil triceratops.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Fósseis</h5>
-                <p class="card-text">Para os curiosos sobre o mundo dos recpteis gigantes, essa seção vai te agradar bastante.</p>
-                <a href="#" class="btn ">Conheça já</a>
-              </div>
+        </div>
+        <div class="col">
+          <div id="card1" class="card">
+            <img id="ths" src="image/fosseil triceratops.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Fósseis</h5>
+              <p class="card-text">Para os curiosos sobre o mundo dos recpteis gigantes, essa seção vai te agradar bastante.</p>
+              <a href="#" class="btn ">Conheça já</a>
             </div>
           </div>
-          <div class="col">
-            <div id="card1" class="card">
-              <img id="ths" src="image/documentodepesquisa.jpeg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Pesquisa Histórica</h5>
-                <p class="card-text">Nesse campo, jovem pesquisador, você irá encontrar alguns documentos de pesquisa.</p>
-                <a href="#" class="btn">Conheça já</a>
-              </div>
+        </div>
+        <div class="col">
+          <div id="card1" class="card">
+            <img id="ths" src="image/documentodepesquisa.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pesquisa Histórica</h5>
+              <p class="card-text">Nesse campo, jovem pesquisador, você irá encontrar alguns documentos de pesquisa.</p>
+              <a href="#" class="btn">Conheça já</a>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   </div>
 
 </section>
